eca759cb44c9f70bbdd1dd06b7285be3d0b9742f
Deivison Nogueira <tp.deivison.nogueira@totvspartners.com.br>
2023-04-01 17:59:47 -0300

    active db dev

--- src/main/java/com/ifms/softmed/config/DevConfig.java
+++ src/main/java/com/ifms/softmed/config/DevConfig.java
@@ -1,10 +1,29 @@
 package com.ifms.softmed.config;
 
-/* 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ifms.softmed.services.DBService;
+
 @Configuration
 @Profile("dev")
 public class DevConfig {
-    
-    
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @Bean
+    public boolean instaciaDB() {
+        if (value.equals("create")) {
+            this.dbService.instanciaDb();
+        }
+       return false;
+    }
+
 }
-*/
